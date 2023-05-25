7a5195dc6943b895428ae86d955d42de5dd15d0a
WayneWeng95 <wayneweng9508@gmail.com>
2023-05-25 19:04:34 +0200

    Adding arrow testing

--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,34 @@ fn running(datasize: i32) {
 }
 
 use arrow::ipc::Bool;
-use arrow::{array, record_batch};
+use arrow::{array, buffer, record_batch};
 use std::sync;
 fn arrow_functional(datasize: i32) {
     //A simple arrow test to testify functionality
 
-    println!("Arrow test");
-    let col_1 = sync::Arc::new(array::Int32Array::from_iter(0..datasize)) as _;
-    let col_2 = sync::Arc::new(array::Int32Array::from_iter(0..datasize)) as _;
+    // println!("Arrow test");
+    // let col_1 = sync::Arc::new(array::Int32Array::from_iter(0..datasize)) as _;
+    // let col_2 = sync::Arc::new(array::Int32Array::from_iter(0..datasize)) as _;
 
-    let batch =
-        record_batch::RecordBatch::try_from_iter([("col1", col_1), ("col_2", col_2)]).unwrap();
-    println!("{:?}", batch);
+    // let batch =
+    //     record_batch::RecordBatch::try_from_iter([("col1", col_1), ("col_2", col_2)]).unwrap();
+    // println!("{:?}", batch);
+
+    let mut buffer = buffer::MutableBuffer::new(0);
+    buffer.push(256u32);
+    buffer.extend_from_slice(&[1u32]);
+    let mut n = 0;
+
+    while n < datasize {
+        // thread::sleep(time::Duration::from_secs(1));
+        buffer.push(n);
+
+        n += 1;
+    }
+
+    let buffer: buffer::Buffer = buffer.into();
+
+    // println!("{:?}", buffer);
 }
 
 use brug;
@@ -118,36 +134,38 @@ use std::{thread, time};
 fn main() {
     println!("I'm using the library:");
 
-    let datasize = 1000000000;
+    let datasize = 1000000;
 
-    let allocator = brug::Allocatormode::_SYS_;
+    // let allocator = brug::Allocatormode::_SYS_;
     // let allocator = brug::Allocatormode::_JEMALLOC_;
     // let allocator = brug::Allocatormode::_MIMALLOC_;
     // let allocator = brug::Allocatormode::_MMAP_;
     // let allocator = brug::Allocatormode::_BrugTemplate_;
-    // let allocator = brug::Allocatormode::_BrugAutoOpt_;
+    let allocator = brug::Allocatormode::_BrugAutoOpt_;
 
     // running(datasize);
-    // arrow_functional(datasize);
+
     // read_file_vec("/home/weikang/Documents/Brug/Wikidump/enwiki-20230201-pages-articles-multistream1.xml-p1p41242").unwrap();
     // read_file_buffer("/home/weikang/Documents/Brug/Wikidump/test.xml");
 
     unsafe {
-        // brug::BrugStruct::enable_monitor();
+        brug::BrugStruct::enable_monitor();
         brug::BrugStruct::set_mode(allocator);
 
         let mut n = 0;
 
         while n < 15 {
-            // thread::sleep(time::Duration::from_secs(1));
-            read_file_buffer("/home/weikang/Documents/Brug/Wikidump/test.xml");
-
-            println!("      ");
+            thread::sleep(time::Duration::from_secs(1));
+            //     read_file_buffer("/home/weikang/Documents/Brug/Wikidump/test.xml");
+            arrow_functional(datasize);
+            //     println!("      ");
 
             n += 1;
         }
 
         brug::BrugStruct::end_set();
+        brug::BrugStruct::monitor_print();
+        brug::BrugStruct::disable_monitor();
     }
 }
 
